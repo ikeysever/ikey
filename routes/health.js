d2428c00d38279aa7c03ac38b2b2394a9061d159
ikeysever <ikeydaangaogong@gmail.com>
2026-09-17 19:01:09 +0800

    Update health.js

--- routes/health.js
+++ routes/health.js
@@ -1,1 +1,13 @@
+const express = require("express");
 
+const router = express.Router();
+
+router.get("/ping", (req, res) => {
+  res.json({
+    status: "ok",
+    service: "iKey",
+    message: "iKey Server OK"
+  });
+});
+
+module.exports = router;
